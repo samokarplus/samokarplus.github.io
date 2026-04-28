63a2607e8dabcee0d0204b00b74667c7372a3761
samokarplus <samokarplus@gmail.com>
2026-04-27 23:48:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -23,14 +23,14 @@ const prompts = [
     author: "Nisargadatta Maharaj",
     quote:
       "Wisdom tells me I am nothing. Love tells me I am everything. And between the two, my life flows.",
-    image: "assets/images/teachers/nisargadatta-maharaj-v5.jpg",
+    image: "assets/images/teachers/nisargadatta.jpg",
     alt: "Portrait of Nisargadatta Maharaj seated outdoors."
   },
   {
     author: "Anandamayi Ma",
     quote:
       "To attain the Truth one has to endure hardships. It is the obstacles that give birth to patience.",
-    image: "assets/images/teachers/anandamayi-ma-v5.jpg",
+    image: "assets/images/teachers/anandamayi.jpg",
     alt: "Portrait of Anandamayi Ma."
   },
   {
@@ -43,13 +43,13 @@ const prompts = [
     author: "Nisargadatta Maharaj",
     quote:
       "The very idea of going beyond the dream is illusory. Why go anywhere? Just realise that you are dreaming a dream you call the world, and stop looking for ways out.",
-    image: "assets/images/teachers/nisargadatta-maharaj-v5.jpg",
+    image: "assets/images/teachers/nisargadatta.jpg",
     alt: "Portrait of Nisargadatta Maharaj seated outdoors."
   },
   {
     author: "Anandamayi Ma",
     quote: "Whether you accept or reject Him, He is there all the same.",
-    image: "assets/images/teachers/anandamayi-ma-v5.jpg",
+    image: "assets/images/teachers/anandamayi.jpg",
     alt: "Portrait of Anandamayi Ma."
   }
 ];
