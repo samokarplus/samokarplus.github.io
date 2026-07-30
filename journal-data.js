e28bb532cfefb6d05ee6e4498a803091b89cc630
samokarplus <samokarplus@gmail.com>
2026-07-30 15:19:13 -0600

    Add new journal entry for July 30, 2026

--- journal-data.js
+++ journal-data.js
@@ -8,6 +8,10 @@
 // Put a blank line between paragraphs.`
 // },
 window.journalEntriesData = [
+{    
+     date: "07-30-26",
+     text: `One semester at a time, I will get these degrees. RAHHHHHHHHHH. One day I'm finna make it out the hood and people will be wondering: how'd that wil' child do it?``
+},
 {
     date: "07-29-26",
     text: `Feel like I'm not doing enough in life right now. Need more risk. I have an idea. Hmmm.`
