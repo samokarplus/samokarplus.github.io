f1454fe4e6fe74ae7a9f2055a49e70cbefa137b0
samokarplus <samokarplus@gmail.com>
2026-08-14 11:57:31 -0600

    Update journal-data.js

--- journal-data.js
+++ journal-data.js
@@ -9,7 +9,7 @@
 // },
 window.journalEntriesData = [
 {    date: "08-14-26",
-     text: `I like dinosaurs. They are cool. The earth's history is completely amazing and crazy. Imagine getting dropped in the ending stages of the Cretaceous period, when the dinosaurs evolved to crazy ones and you're just walking around a desert while an Ankylosaurus charges at you and you died. That would be so coooool 😎🦖🦕`
+     text: `I like dinosaurs. They are cool. The earth's history is completely amazing and crazy. Imagine getting dropped in the ending stages of the Cretaceous period, when the dinosaurs evolved to crazy ones and you're just walking around a desert while an Ankylosaurus charges at you and you died. Science please bring back the Yangchuanosaurus!!!! That would be so coooool 😎🦖🦕`
 },
 {
      date: "08-10-26",
