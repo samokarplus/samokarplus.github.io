4950a18d3d89544ce346d69dbe891399cda61cd7
samokarplus <samokarplus@gmail.com>
2026-09-17 15:30:00 -0600

    Add new journal entry on love and self-awareness
    Added a new journal entry about relationships and personal reflections.

--- journal-data.js
+++ journal-data.js
@@ -8,6 +8,9 @@
 // Put a blank line between paragraphs.`
 // },
 window.journalEntriesData = [
+       {date: "09-17-26",
+        text: `I enjoy dating someone who knows biology and rockets. Two things I have no idea about. Actually blessed. I would not want to be with someone so similar to me.`
+       },
      {date: "09-12-26",
       text: `The only way out is through. The only way through is LOVE. Love requires honesty, devotion, acceptance, openness, truth. Sometimes our fears, thoughts, and restrictions can cloud our judgment upon the world, ourselves, our neighbors, and even people we love the most. When we do less fear-based judgment and restriction and more honesty and kindness without neglecting reality, we heal. Be open. Be love. Love is not a feeling, thought, emotion, or expression but rather a constant reality that exists. If only I could quiet my mind to listen to that which is always ringing loudly, but yet sometimes I subconsciously obstruct my ears and soul. My former spiritual teacher's mother always told him: Remember that which you forgot to remember. What a great reminder.
       
